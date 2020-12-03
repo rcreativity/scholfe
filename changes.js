c92ffd134a124b426578f17521ccda8be34005ba
Ravikumar <ravikumar@Ravikumars-MacBook-Pro.local>
2020-12-04 03:40:26 +0530

    new commit

---
+++ changes.js
@@ -0,0 +1,11 @@
+/**
+ * logo little bigger ✅
+ * about us new page needs to open and ✅
+ * teams also needs to inside about us ✅
+ * login new page with form (parent and institure seprate page and form) ✅ ❌ (add styles)
+ * need to add FAQ (same page) ✅
+ * Services Overview - need to removed and instead FAQ Section ✅
+ * Not Sure Which Plan is Right For You? => remove this section ✅
+ * Get a Loan In 3 Quick Steps => turn this section into blog section ✅
+ * contact us (google map) inside footer ❌
+ */
